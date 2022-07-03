2dead9c73b7482a63eaa5d99262090140a352cd9
Myudro33 <qanashvili.myudro@gmail.com>
2022-07-03 19:06:51 +0400

    change key

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <Box mt='80px' bgColor='#fff3f4'>
       <Stack gap='40px' alignItems='center' px='40px' pt='24px' >
         <img src={Logo} alt='logo' width='200px' height='40px' />
-        <Typography variant='h5' pb='40px' mt='20px'>Made with ❤️ by Nika :)</Typography>
+        <Typography variant='h5' pb='40px' mt='20px'>Made with ❤️ by not me :)</Typography>
       </Stack>
     </Box>
   )
